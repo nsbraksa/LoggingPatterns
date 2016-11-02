fbad29b4361bf655f4f8433b224f64e776e136ca
Zakaria Braksa <zakaria.braksa@gmail.com>
2016-11-02 22:02:58 +0000

    init

---
+++ LoggingPatterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoggingPatterns
+//
+//  Created by Zakaria Braksa on 11/2/16.
+//  Copyright © 2016 Zakaria Braksa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoggingPatterns/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoggingPatterns
+//
+//  Created by Zakaria Braksa on 11/2/16.
+//  Copyright © 2016 Zakaria Braksa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
